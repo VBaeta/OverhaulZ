311ada97f45e3f2f16b7d96bd29041d06e06610f
ViniTheKidd <vbaeta2015@gmail.com>
2025-01-09 06:18:49 -0300

    Initial commit

---
+++ src/main/java/net/vinithekidd/overhaulz/OverhaulZClient.java
@@ -0,0 +1,10 @@
+package net.vinithekidd.overhaulz;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OverhaulZClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
